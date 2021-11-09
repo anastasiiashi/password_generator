e70668e8a06fa51c1bb935ee1cc9ae77a845b7dc
anastasiiashi <anastasia.p.morozova@gmail.com>
2021-11-09 23:48:09 +0200

    add generator and copy generated password functions

--- generator.py
+++ generator.py
@@ -6,26 +6,29 @@ root_window.geometry("400x300")
 root_window.title("Password generator")
 
 
+def new_rand():
+    psw_output.delete(0, END)
+    psw_len = int(input_characters.get())
+    password = ''
+    for i in range(psw_len):
+        password += chr(randint(33, 126))
+    psw_output.insert(0, password)
 
-number_of_characters = LabelFrame(root_window,
-                             text="Сколько символов?",
-                             font=("arial"))
+
+def copy_password():
+    root_window.clipboard_clear()
+    root_window.clipboard_append(psw_output.get())
+
+
+number_of_characters = LabelFrame(root_window, text="Сколько символов?",
+                                  font=("arial"))
 number_of_characters.pack(pady=20)
 
 input_characters = Entry(number_of_characters, font=("arial", 14))
 input_characters.pack(pady=20, padx=20)
 
-psw_output = Entry(root_window, text="", font=("arial", 14))
-psw_output.pack(pady=20)
-
-
-def new_rand():
-    input_characters.delete(0, END)
-    psw_len = int(psw_output.get())
-    new_password = chr(randint(33, 126))
-
-def copy_password():
-    pass
+psw_output = Entry(root_window, text="", font=("arial", 14), bd=0, bg="SystemButtonFace")
+psw_output.pack(pady=10)
 
 
 frame_for_buttons = Frame(root_window)
